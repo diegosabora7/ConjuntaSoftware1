a028b8046d5663bcbf13ebe7067ddf34bce0a33d
DiegoYandun <DiegoYandun@10.9.4.67>
2017-07-12 20:32:10 -0500

    Creacion de las operaciones suma y resta

--- src/main/java/ec/edu/espe/isi/conjunta/conjuntasoftware1/OperacionesFloat.java
+++ src/main/java/ec/edu/espe/isi/conjunta/conjuntasoftware1/OperacionesFloat.java
@@ -11,22 +11,40 @@ package ec.edu.espe.isi.conjunta.conjuntasoftware1;
  * @author Pablo Guallichico
  */
 public class OperacionesFloat {
-        /**
-     * Metodo para la hacer una multiplicacion entre dos numeros.
+     /**
+     * Metodo para realizar la suma entre dos numeros flotantes.
+     * @param a, operando a de la suma.
+     * @param b, operando b de la suma.
+     * @return resultado de la operacion.
+     */
+    public float suma(float a, float b){
+        return a+b;
+    }
+    /**
+     * Metodo para realizar la resta entre dos numeros enteros.
+     * @param a, operando a de la resta.
+     * @param b, operando b de la resta.
+     * @return resultado de la operacion.
+     */
+    public float resta(float a, float b){
+        return a-b;
+    } 
+    /**
+     * Metodo para la hacer una multiplicacion entre dos numeros flotantes.
      * @param a, multiplicando de la operacion.
      * @param b, multiplicador de la opracion.
      * @return resultado de la operacion.
      */
-    public int multiplicacion(int a, int b){
+    public float multiplicacion(float a, float b){
         return a*b;
     }
     /**
-     * Metodo para la hacer una division entre dos numeros.
+     * Metodo para la hacer una division entre dos numeros flotantes.
      * @param a, dividendo de la operacion.
      * @param b, divisor de la opracion.
      * @return resultado de la operacion.
      */
-    public int division(int a, int b){
+    public float division(float a, float b){
         return a/b;
     }
 }
