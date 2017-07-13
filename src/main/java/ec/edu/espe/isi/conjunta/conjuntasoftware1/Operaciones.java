a4658ceec1efb95fdb871b9598fe160b7a90bdca
Educatespe <educatespe@gmail.com>
2017-07-12 20:14:58 -0500

    Creacion de lo metodos para la multiplicacion y division

--- src/main/java/ec/edu/espe/isi/conjunta/conjuntasoftware1/Operaciones.java
+++ src/main/java/ec/edu/espe/isi/conjunta/conjuntasoftware1/Operaciones.java
@@ -6,9 +6,27 @@
 package ec.edu.espe.isi.conjunta.conjuntasoftware1;
 
 /**
- *
+ * Clase para el control de operaciones entre dos números.
  * @author DiegoYandun
+ * @author Pablo Guallichico
  */
 public class Operaciones {
-    
+    /**
+     * Metodo para la hacer una multiplicacion entre dos numeros.
+     * @param a, multiplicando de la operacion.
+     * @param b, multiplicador de la opracion.
+     * @return resultado de la operacion.
+     */
+    public int multiplicacion(int a, int b){
+        return a*b;
+    }
+    /**
+     * Metodo para la hacer una division entre dos numeros.
+     * @param a, dividendo de la operacion.
+     * @param b, divisor de la opracion.
+     * @return resultado de la operacion.
+     */
+    public int division(int a, int b){
+        return a/b;
+    }
 }
